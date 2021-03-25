e206d286ff45a78bd0e4d1859acc421c50d74bf0
Majra <majra.oruc@gmail.com>
2021-03-25 23:47:56 +0100

    Fix in ex08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -2,12 +2,14 @@
 let one = new Set([1, 2, 3, 4, 5]);
 let two = new Set([6, 4, 3, 2]);
 let unionSet = new Set([...one, ...two]);
-console.log(unionSet);
 let intersectionSet = new Set([...one].filter(x => two.has(x)));
-console.log(intersectionSet);
 let differenceSet = new Set([...one].filter(x => !two.has(x)));
-console.log(differenceSet);
 
+console.log(one);
+console.log(two);
+console.log(unionSet);
+console.log(intersectionSet);
+console.log(differenceSet);
 // Only change code above this line
 
 module.exports = { unionSet, intersectionSet, differenceSet }
